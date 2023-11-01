69ac044e64acece61d780c56ede57c155c42fbaf
Decagon <wikiwoo@wikiwoos-MacBook-Pro.local>
2023-11-01 13:19:45 +0100

    first commit

--- src/classifier.js
+++ src/classifier.js
@@ -1,3 +1,98 @@
-function classifier(input) {}
+function classifier(input) {
+    
+  if (input.length === 0) {
+    // Handle empty input array by returning an empty result
+    return { noOfGroups: 0 };
+  }
 
+// Create a copy of the input array to avoid modifying the original array
+const inputCopy = input.slice();
+
+function calculateAge(dob) {
+const currentDate = new Date('2019-01-01'); // Assuming the current year is 2019
+const birthDate = new Date(dob);
+const ageInYears = currentDate.getFullYear() - birthDate.getFullYear();
+return ageInYears;
+}
+
+inputCopy.sort((a, b) => calculateAge(a.dob) - calculateAge(b.dob));
+
+const groups = {};
+let groupCounter = 1;
+const maxGroupSize = 3;
+const maxAgeDifference = 5;
+
+for (const student of inputCopy) {
+const age = calculateAge(student.dob);
+let assigned = false;
+
+for (let i = 1; i <= groupCounter; i++) {
+  const groupKey = `group${i}`;
+  const group = groups[groupKey];
+
+  if (!group) {
+    groups[groupKey] = {
+      members: [
+        {
+          name: student.name,
+          dob: student.dob,
+          regNo: student.regNo,
+          age,
+        },
+      ],
+      oldest: age,
+      sum: age,
+      regNos: [parseInt(student.regNo)],
+    };
+    assigned = true;
+    break;
+  }
+
+  if (
+    group.members.length < maxGroupSize &&
+    Math.abs(group.oldest - age) <= maxAgeDifference
+  ) {
+    group.members.push({
+      name: student.name,
+      dob: student.dob,
+      regNo: student.regNo,
+      age,
+    });
+    group.members.sort((a, b) => a.age - b.age);
+    group.oldest = group.members[group.members.length - 1].age;
+    group.sum += age;
+    group.regNos.push(parseInt(student.regNo));
+    group.regNos.sort((a, b) => a - b); // Sort regNos in ascending order
+    assigned = true;
+    break;
+  }
+}
+
+if (!assigned) {
+  groupCounter++;
+  groups[`group${groupCounter}`] = {
+    members: [
+      {
+        name: student.name,
+        dob: student.dob,
+        regNo: student.regNo,
+        age,
+      },
+    ],
+    oldest: age,
+    sum: age,
+    regNos: [parseInt(student.regNo)],
+  };
+}
+}
+
+const noOfGroups = groupCounter;
+
+const output = { noOfGroups };
+for (const groupKey in groups) {
+output[groupKey] = groups[groupKey];
+}
+
+return output;
+}
 export default classifier;
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,3 +1,51 @@
-function findDuplicateTransactions(transactions) {}
-
+function findDuplicateTransactions(transactions) {
+    const groupedTransactions = {};
+ 
+    // Create a unique key for each transaction based on sourceAccount, targetAccount, category, and amount
+    transactions.forEach((transaction) => {
+        const key = `${transaction.sourceAccount}-${transaction.targetAccount}-${transaction.category}-${transaction.amount}`;
+        if (!groupedTransactions[key]) {
+            groupedTransactions[key] = [transaction];
+        } else {
+            groupedTransactions[key].push(transaction);
+        }
+    });
+ 
+    // Find and collect groups of transactions with time difference less than 1 minute
+    const duplicateGroups = [];
+ 
+    for (const key in groupedTransactions) {
+        const group = groupedTransactions[key];
+        if (group.length > 1) {
+            group.sort((a, b) => new Date(a.time) - new Date(b.time));
+            const timeThreshold = 60 * 1000; // 1 minute in milliseconds
+            let currentGroup = [group[0]];
+ 
+            for (let i = 1; i < group.length; i++) {
+                const prevTime = new Date(currentGroup[currentGroup.length - 1].time);
+                const currentTime = new Date(group[i].time);
+                const timeDifference = Math.abs(currentTime - prevTime);
+ 
+                if (timeDifference <= timeThreshold) {
+                    currentGroup.push(group[i]);
+                } else {
+                    if (currentGroup.length > 1) {
+                        duplicateGroups.push(currentGroup);
+                    }
+                    currentGroup = [group[i]];
+                }
+            }
+ 
+            if (currentGroup.length > 1) {
+                duplicateGroups.push(currentGroup);
+            }
+        }
+    }
+ 
+    // Sort groups by the time of the first transaction in each group
+    duplicateGroups.sort((a, b) => new Date(a[0].time) - new Date(b[0].time));
+ 
+    return duplicateGroups;
+}
+ 
 export default findDuplicateTransactions;
